dd62e151d58b0f94e254f8db7151c6df34b8bad9
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2023-05-13 13:10:20 +0300

    Initial commit

--- src/Draft0_Whole_Video_Case2.java
+++ src/Draft0_Whole_Video_Case2.java
@@ -1,5 +1,10 @@
 public class Draft0_Whole_Video_Case2 {
     public static void main(String[] args) {
+        System.out.println("Задание: \nПользователь вводит месяц и год.\n" +
+                "Скачать все снимки за месяц в папку.\n" +
+                "Сгенерировать html страницу в этой папке, которая отобразит все скачанные снимки на одной странице. Пример:\n" +
+                "<img src=“1.png”/>\n" +
+                "<img src=“2.png”/>\n\nРешение: ");
 
     }
 
